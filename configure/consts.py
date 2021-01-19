203ec890d776b8e3fc29905e0dd7848f6a4b9560
Neel Gala <neelgala@incoresemi.com>
2021-01-19 18:01:45 +0530

    checkout caches v9.0.0

--- configure/consts.py
+++ configure/consts.py
@@ -53,7 +53,7 @@ include depends.mk
 dependency_yaml='''
 caches_mmu:
   url: https://gitlab.com/shaktiproject/uncore/caches_mmu
-  checkout: csrbox-integration
+  checkout: 9.0.0
 common_bsv:
   url: https://gitlab.com/shaktiproject/common_bsv
   checkout: master
