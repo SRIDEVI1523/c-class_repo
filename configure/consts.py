5f59d496d6204576d13c500317938c49d56c408c
Venkatakrishnan Sutharsan <venkatakrishnan.sutharsan@gmail.com>
2022-07-27 18:32:52 +0530

    Changes to simulate c-class

--- configure/consts.py
+++ configure/consts.py
@@ -59,7 +59,7 @@ common_bsv:
   checkout: master
 devices:
   url: https://gitlab.com/shaktiproject/uncore/devices
-  checkout: 5.0.1
+  checkout: sos
 fabrics:
   url: https://gitlab.com/shaktiproject/uncore/fabrics
   checkout: 1.2.0
