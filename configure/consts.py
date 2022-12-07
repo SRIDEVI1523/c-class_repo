267de7b14d65806063b68b1c4829c4cca1e8acfe
P Girinath <palanigirinath@gmail.com>
2022-12-07 13:31:12 +0000

    Update configure/consts.py

--- configure/consts.py
+++ configure/consts.py
@@ -68,7 +68,7 @@ common_verilog:
   checkout: master
 verification:
   url: https://gitlab.com/shaktiproject/verification_environment/verification
-  checkout: 4.0.22
+  checkout: 73-update-for-cclass
   recursive: True
   patch:
     - [riscv-tests/env , verification/patches/riscv-tests-shakti-signature.patch]
