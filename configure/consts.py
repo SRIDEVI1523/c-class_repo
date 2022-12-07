04dd9d09f1f47df9f58dfa4d757974b34cea19be
Lavanya J <lavanya.jagan@gmail.com>
2022-12-07 04:44:25 +0000

    Update configure/consts.py

--- configure/consts.py
+++ configure/consts.py
@@ -68,7 +68,7 @@ common_verilog:
   checkout: master
 verification:
   url: https://gitlab.com/shaktiproject/verification_environment/verification
-  checkout: 4.0.19
+  checkout: 4.0.21
   recursive: True
   patch:
     - [riscv-tests/env , verification/patches/riscv-tests-shakti-signature.patch]
